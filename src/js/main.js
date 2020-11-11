032cc98f3e14661324096541f7f3a90399933a16
Vlad <heremymale@gmail.com>
2020-11-11 14:06:07 +0200

    created videoplayer based on youtube api for first page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from './modules/slider';
+import VideoPlayer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
     const sliderMain = new Slider('.page', '.next');
     sliderMain.render();
+
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
 });
