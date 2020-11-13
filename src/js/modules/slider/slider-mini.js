c80acaf2eaef8044915a2a3e8dbc9f92f4f1a114
Vlad <heremymale@gmail.com>
2020-11-13 10:46:03 +0200

    created logic for all sliders on the first page

---
+++ src/js/modules/slider/slider-mini.js
@@ -0,0 +1,49 @@
+import Slider from './slider';
+
+export default class MiniSlider extends Slider {
+    constructor(container, next, prev, activeClass, animate, autoplay) {
+        super(container, next, prev, activeClass, animate, autoplay);
+    }
+    decorizeSlides() {
+        this.slides.forEach(slide => {
+            slide.classList.remove(this.activeClass);
+            if (this.animate) {
+                slide.querySelector('.card__title').style.opacity = '0.4';
+                slide.querySelector('.card__controls').style.opacity = '0.4';
+            }
+        });
+
+        this.slides[0].classList.add(this.activeClass);
+
+        if (this.animate) {
+            this.slides[0].querySelector('.card__title').style.opacity = '1';
+            this.slides[0].querySelector('.card__controls').style.opacity = '1';
+        }
+    }
+    moveRight() {
+        this.next.addEventListener('click', () => {
+            let current = this.slides.shift();
+            this.container.append(current);
+            this.slides.push(current);
+            this.decorizeSlides();
+        });
+    }
+    moveLeft() {
+        this.prev.addEventListener('click', () => {
+            let current = this.slides.pop();
+            this.container.prepend(current);
+            this.slides.unshift(current);
+            this.decorizeSlides();
+        });
+    }
+    init() {
+        this.container.style.cssText = `
+            display: flex;
+            flex-wrap: wrap;
+            overflow: hidden;
+            align-items: flex-start;
+        `;
+        this.moveRight();
+        this.moveLeft();
+    }
+}
--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -1,8 +1,21 @@
 export default class Slider {
-    constructor({page = "", btns = "", next = "", prev = ""} = {}) {
-        this.page = document.querySelector(page);
-        this.slides = this.page.children;
+    constructor({
+        container = null,
+        btns = null,
+        next = null,
+        prev = null,
+        activeClass = '',
+        animate = false,
+        autoplay = false
+        } = {}) {
+        this.container = document.querySelector(container);
+        this.slides = Array.from(this.container.children).filter(slide => slide.localName != 'button');
         this.btns = document.querySelectorAll(btns);
+        this.prev = document.querySelector(prev);
+        this.next = document.querySelector(next);
+        this.activeClass = activeClass;
+        this.animate = animate;
+        this.autoplay = autoplay;
         this.slideIndex = 1;
     }
 }
